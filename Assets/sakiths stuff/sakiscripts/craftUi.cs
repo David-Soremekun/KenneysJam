7f88765386fb6d2c075f197346221ee3c23c50ed
GioTheGreen <B1050622@live.tees.ac.uk>
2023-07-21 23:46:19 +0100

    slowly learning
    im almost out of exile

---
+++ Assets/sakiths stuff/sakiscripts/craftUi.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class craftUi : MonoBehaviour
+{
+    public bool SetFordelete = false;
+    public GameObject owner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SetFordelete)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void setOwner(GameObject o)
+    {
+        owner = o;
+    }
+    public void pressed()
+    {
+        Debug.Log("pressed");
+    }
+}
---
+++ Assets/sakiths stuff/sakiscripts/crafting.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crafting : MonoBehaviour
+{
+    public bool selected; //see if this is currently selected craft
+    public bool canCraft;
+    public bool crafted;
+    public string item;
+
+    public BoxCollider2D col;
+    public GameObject spwan;
+    public GameObject o;
+
+    public int WoodHave;
+    public int IronHave;
+    public int GoldHave;
+    public int MythrilHave;
+
+
+    private int WoodNeeded;
+    private int IronNeeded;
+    private int GoldNeeded;
+    private int MythrilNeeded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canCraft = false;
+        selected = false;
+        item = "Iron_sward";
+        WoodNeeded = 2;
+        IronNeeded = 5;
+        GoldNeeded = 0;
+        MythrilNeeded = 0;
+        spwan.GetComponent<craftUi>().setOwner(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((WoodHave >= WoodNeeded)&&(IronHave >= IronNeeded)&&(GoldHave >= GoldNeeded)&&(MythrilHave >= MythrilNeeded))
+        {
+            canCraft = true;
+        }
+        else
+        {
+            canCraft = false;
+        }
+    }
+
+    //collition
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        selected = true;
+        o = Instantiate(spwan, new Vector3(transform.position.x + 1, transform.position.y + 1, transform.position.z), transform.rotation);
+        
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        selected = false;
+        o.GetComponent<craftUi>().SetFordelete = true;
+    }
+}
